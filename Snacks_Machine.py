2384cd05ac99e5f325f5fbfefb0a0a2d9d0a96b3
ExZero777 <ldmarques007@gmail.com>
2020-11-08 21:30:31 -0300

    se redirecciona salir a main en gestion y se corrigen errores en main

--- Snacks_Machine.py
+++ Snacks_Machine.py
@@ -8,7 +8,7 @@ cantidad_snacks=""
 precio_snacks=""
 
 snacks = [nombre_snacks,cantidad_snacks,precio_snacks]    
-    
+
 codigo_ingreso=""
 nombre_ingreso=""
 apellido_ingreso=""
@@ -31,6 +31,8 @@ gestion_totales=""
 total_totales=""
     
 totales = [codigo_totales,fecha_totales,horario_totales,gestion_totales,total_totales]    
+    
+    
     
 def totales(opci):
     ft=open("totales.txt","r")
@@ -76,101 +78,101 @@ def totales(opci):
 
 def gestion(ge,o):
     if(o==0):
-       # El usuario admin genera un ingreso del nuevo snack, con precio y cantidad disponible
-       if(ge==1):
-          f = open("snacks.txt","a")
-          nombre_snacks = input(str("Ingrese El Nuevo Snack: "))
-          precio_snacks = input(str("Ingrese El Precio Del Snack: "))
-          cantidad_snacks= input(str("Ingrese La Cantidad Del Snack: "))
-          f.write(nombre_snacks+","+precio_snacks+","+cantidad_snacks+"\n")
-          print("Ingreso Exitoso")
-          f.close()
+        # El usuario admin genera un ingreso del nuevo snack, con precio y cantidad disponible
+         if(ge==1):
+            f = open("snacks.txt","a")
+            nombre_snacks = input(str("Ingrese El Nuevo Snack: "))
+            precio_snacks = input(str("Ingrese El Precio Del Snack: "))
+            cantidad_snacks= input(str("Ingrese La Cantidad Del Snack: "))
+            f.write(nombre_snacks+","+precio_snacks+","+cantidad_snacks+"\n")
+            print("Ingreso Exitoso")
+            f.close()
     
-       # Se abre el archivo snacks.txt y el usuario admin puede eliminar cantidad de snack disponibles
-       if(ge==2):
-          f = open("snacks.txt","r")
-          i = 0   
-          for aline in f:
-             values = aline.split()
-             print(" %4d: %s" %(i, values))
-             i+=1
-          f.close()
-          
-          f = open("snacks.txt","r")
-          lines = f.readlines()
-          f.close()
-        
-          lineas= input("Ingrese El Número De Snack A Borrar!!: ")
-          del lines[int(lineas)]
-          f = open("snacks.txt","w")
-          for line in lines:
-              f.write(line)
-              print("Borrado Exitoso ")
-          else:
-             print("Error,Ingrese Nuevamente El Indice") 
-          f.close()
-       
-       #El usuario admin puede ingresar y ver el listado de snacks existentes          
-       if(ge==3):
-          print("\033[1;35m"+"\n  -----"+'\033[0m'+'\033[1m'+"Lista De Snacks"+'\033[0m'+"\033[1;35m"+"-----"+'\033[0m')
-          print("\033[;35m"+"       ═════ ══ ══════     "+'\033[0;m')
-          f = open("snacks.txt")
-          i = 1
-          for linea in f:
-              linea = linea.rstrip("\\n")
-              print(" %4d: %s" %(i, linea))
-              i+=1
-          f.close()
-                  
-       #El usuario admin abre el archivo ingreso.txt y se le permite ingresar un nuevo usuario al mismo
-       if(ge==4):
-          f = open("ingreso.txt","a")
-          codigo_ingreso= input(str("Ingrese El Codigo Del Usuario: "))
-          nombre_ingreso = input(str("Ingrese El Nombre Del Usuario: "))
-          apellido_ingreso = input(str("Ingrese El Apellido Del Usuario: "))
-          dni_ingreso = input(str("Ingrese El Dni Del Usuario: "))
-          permiso_ingreso=input(str("Ingrese El Permiso Del Usuario: "))
-          f.write(codigo_ingreso+","+nombre_ingreso+","+apellido_ingreso+","+dni_ingreso+","+permiso_ingreso+"\n")
-          print("Ingreso Exitoso")
-          f.close()
-        
-       #Le permite al usuario admin borrar un usuario del listado 
-       if(ge==5):
-           f = open("ingreso.txt","r")
-           i = 0   
-           for aline in f:
-              values = aline.split()
-              print(" %4d: %s" %(i, values))
-              i+=1
-           f.close()
-       
-           f = open("ingreso.txt","r")
-           lines = f.readlines()
-           f.close()
-       
-           lineas= input("Ingrese El Codigo Del Usuario A Borrar!!: ")
-           del lines[int(lineas)]
-           f = open("ingreso.txt","w")
-           for line in lines:
-              f.write(line)
-              print("Borrado Exitoso ")
-           else:
-              print("Error,Ingrese Nuevamente El Indice") 
-           f.close()
-
-       #Le permite al usuario admin ver el listado de los usuarios existentes
-       if(ge==6):
-           print("\033[1;35m"+"\n\t-----"+'\033[0m'+'\033[1m'+"Lista De Usuarios"+'\033[0m'+"\033[1;35m"+"-----"+'\033[0m')
-           print("\033[;35m"+"\t     ═════ ══ ════════     "+'\033[0;m')
-           f = open("ingreso.txt")
-           i = 1
-           for linea in f:
-               linea = linea.rstrip("\\n")
-               print(" %4d: %s" %(i, linea))
+         # Se abre el archivo snacks.txt y el usuario admin puede eliminar cantidad de snack disponibles
+         if(ge==2):
+            f = open("snacks.txt","r")
+            i = 0   
+            for aline in f:
+               values = aline.split()
+               print(" %4d: %s" %(i, values))
                i+=1
-           f.close()
-       if(ge==7):
-            main()
+            f.close()
+          
+            f = open("snacks.txt","r")
+            lines = f.readlines()
+            f.close()
+        
+            lineas= input("Ingrese El Número De Snack A Borrar!!: ")
+            del lines[int(lineas)]
+            f = open("snacks.txt","w")
+            for line in lines:
+                f.write(line)
+                print("Borrado Exitoso ")
+            else:
+               print("Error,Ingrese Nuevamente El Indice") 
+            f.close()
+       
+         #El usuario admin puede ingresar y ver el listado de snacks existentes          
+         if(ge==3):
+            print("\033[1;35m"+"\n  -----"+'\033[0m'+'\033[1m'+"Lista De Snacks"+'\033[0m'+"\033[1;35m"+"-----"+'\033[0m')
+            print("\033[;35m"+"       ═════ ══ ══════     "+'\033[0;m')
+            f = open("snacks.txt")
+            i = 1
+            for linea in f:
+                linea = linea.rstrip("\\n")
+                print(" %4d: %s" %(i, linea))
+                i+=1
+            f.close()
+                  
+         #El usuario admin abre el archivo ingreso.txt y se le permite ingresar un nuevo usuario al mismo
+         if(ge==4):
+            f = open("ingreso.txt","a")
+            codigo_ingreso= input(str("Ingrese El Codigo Del Usuario: "))
+            nombre_ingreso = input(str("Ingrese El Nombre Del Usuario: "))
+            apellido_ingreso = input(str("Ingrese El Apellido Del Usuario: "))
+            dni_ingreso = input(str("Ingrese El Dni Del Usuario: "))
+            permiso_ingreso=input(str("Ingrese El Permiso Del Usuario: "))
+            f.write(codigo_ingreso+","+nombre_ingreso+","+apellido_ingreso+","+dni_ingreso+","+permiso_ingreso+"\n")
+            print("Ingreso Exitoso")
+            f.close()
+        
+         #Le permite al usuario admin borrar un usuario del listado 
+         if(ge==5):
+             f = open("ingreso.txt","r")
+             i = 0   
+             for aline in f:
+                values = aline.split()
+                print(" %4d: %s" %(i, values))
+                i+=1
+             f.close()
+       
+             f = open("ingreso.txt","r")
+             lines = f.readlines()
+             f.close()
+       
+             lineas= input("Ingrese El Codigo Del Usuario A Borrar!!: ")
+             del lines[int(lineas)]
+             f = open("ingreso.txt","w")
+             for line in lines:
+                f.write(line)
+                print("Borrado Exitoso ")
+             else:
+                print("Error,Ingrese Nuevamente El Indice") 
+             f.close()
+
+         #Le permite al usuario admin ver el listado de los usuarios existentes
+         if(ge==6):
+             print("\033[1;35m"+"\n\t-----"+'\033[0m'+'\033[1m'+"Lista De Usuarios"+'\033[0m'+"\033[1;35m"+"-----"+'\033[0m')
+             print("\033[;35m"+"\t     ═════ ══ ════════     "+'\033[0;m')
+             f = open("ingreso.txt")
+             i = 1
+             for linea in f:
+                 linea = linea.rstrip("\\n")
+                 print(" %4d: %s" %(i, linea))
+                 i+=1
+             f.close()
+         if(ge==6): 
+             main()
         
     # Se crea un menú que le permite al usuario (cliente) ver el listado de snacks disponibles
     # y que luego seleccione el snack que desee llevar
@@ -188,7 +190,7 @@ def gestion(ge,o):
             print("-----------------------------")
             cg=input("Ingrese La Cantidad De Snacks A Llevar: ")
             if(cg=="0" or cg==""):
-                ingreso()
+                main()
             else:    
                 fs=open("snacks.txt","r")
                 fg=open("gestion.txt","r")
@@ -200,7 +202,7 @@ def gestion(ge,o):
                 fg=open("gestion.txt","a+")
                 fs=open("snacks.txt","w")
                 c=0
-                while (c!=1):
+                while(c!=1):
                     codigo_gestion=str(o)
                     snack_gestion=input("Ingrese El Codigo Del Snack Que Desea: ")
                     lineas=lines[int(snack_gestion)]
@@ -221,7 +223,7 @@ def gestion(ge,o):
                     c+=1
                 else:
                     print("Error,Ingrese Nuevamente El Indice") 
-                
+            totales()    
         
             
         if(ge==2):
@@ -257,13 +259,15 @@ def maquina(op):
             lin="{:4.3}".format(line)
             lini="{:4.3}".format(str(op))
             if(lin==lini):
-                print("linea encontrada")
                 print("\033[0;35m"+"╔══════════"+'\033[0;m'+"Menu"+"\033[0;35m"+"═════════╗")
                 print("║"+"\033[0;36m"+" 1-Snacks Para Llevar  "+'\033[0;m'+"\033[0;35m"+"║")  
                 print("║"+'\033[0;m'+"\033[0;36m"+" 2-Salir               "+'\033[0;m'+"\033[0;35m"+"║")
                 print("╚═══════════════════════╝"+'\033[0;m')
                 ges=int(input("Favor De Ingresar Una Opcion Para Operar: "))
-                gestion(ges,op)
+                if(ges==1):
+                    gestion(ges,op)
+                else:
+                    main()
         else:
             print("El Codigo Ingresado Es Incorrecto, Favor De Ingresarlo Nuevamente O Comunicarse Con RR.HH")
          
@@ -293,6 +297,6 @@ def main():
            main()
         else:
            print("Hasta Luego")
-           exit()
+           main()
  
 main()
